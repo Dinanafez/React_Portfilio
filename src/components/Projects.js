e9b1e605348194a8a13d3a40d18e20bf7e03d913
Dinanafez <nafez.dina@gmail.com>
2025-01-30 02:14:10 +0300

    react v

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,7 +44,6 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>My Project During my traning u=in Orange Coding Academy</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
